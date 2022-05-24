49451f1442f711c342c1ab1d5778b15e61a263ec
Aidyn Omarov <aidyn_o_b@mail.ru>
2022-05-24 11:00:56 +0600

    Finish

--- cache.go
+++ cache.go
@@ -3,21 +3,50 @@ package cache
 import "time"
 
 type Cache struct {
+	Hash map[string]Data
+}
+
+type Data struct {
+	value    string
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		Hash: make(map[string]Data),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	if val, ok := c.Hash[key]; ok {
+		if val.deadline.IsZero() || time.Now().Before(val.deadline) {
+			return val.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.Hash[key] = Data{
+		value:    value,
+		deadline: time.Time{},
+	}
+	return
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var sessions []string
+	for key, value := range c.Hash {
+		if value.deadline.IsZero() || time.Now().Before(value.deadline) {
+			sessions = append(sessions, key)
+		}
+	}
+	return sessions
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.Hash[key] = Data{
+		value:    value,
+		deadline: deadline,
+	}
 }
